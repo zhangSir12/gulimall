732d19b41e5f94e8806b71c44b0b3939c7d5ecba
zhang <zhangzihanboy@163.com>
2023-02-21 19:39:04 +0800

    项目初始结构创建

---
+++ gulimall-wall/src/main/java/com/atguigu/gulimallwall/GulimallWallApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimallwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWallApplication.class, args);
+    }
+
+}
---
+++ gulimall-wall/src/test/java/com/atguigu/gulimallwall/GulimallWallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimallwall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
