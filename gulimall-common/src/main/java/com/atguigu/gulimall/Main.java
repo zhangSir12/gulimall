9325406213ea6ebeb39e0d37e8bb9eed7b1b364f
zhang <zhangzihanboy@163.com>
2023-02-23 09:14:41 +0800

    项目初始结构创建

---
+++ gulimall-common/src/main/java/com/atguigu/gulimall/Main.java
@@ -0,0 +1,12 @@
+package com.atguigu.gulimall;
+
+/**
+ * Author：张Sir
+ * Date： ${DATE} ${TIME}
+ * Slogan：致敬大师，致敬自己
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
